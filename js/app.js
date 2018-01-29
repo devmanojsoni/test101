5f51209f897189a4cfdf5c4d4b175cdd9071048a
devmanojsoni <er.manojsoni@hotmail.com>
2018-01-30 01:04:31 +0530

    app123.js

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// "use strict";
+var x = 500;
+
+
+function test() {
+    var cx = 999;
+    var y = 100;
+    console.log("Value of Y " + y + x);
+}
+
+test();
+alert("hi");
+console.log(x);
+console.log("hi there");
